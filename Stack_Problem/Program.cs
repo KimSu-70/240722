2384d638446a8a1da9104dc3c824ed0282c4d27b
KimSu-70 <rlatngh4770@naver.com>
2024-08-01 09:48:01 +0900

    스택과 큐 과제

---
+++ Stack_Problem/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Stack_Problem
+{
+    internal class Program
+    {
+        public static bool Test1(char input, char inout)
+        {
+            if (input == '(' && inout == ')')
+                return true;
+            if (input == '[' && inout == ']')
+                return true;
+            if (input == '{' && inout == '}')
+                return true;
+            return false;
+        }
+        
+        
+        public static bool Test2(string PV)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char a in PV)
+            {
+                if(a=='('||a=='['||a=='{')
+                {
+                    stack.Push(a);
+                }
+                else if (a==')'||a==']'|| a== '}')
+                {
+                    if (stack.Count ==0 || !Test1(stack.Peek(),a))
+                    {
+                        return false;
+                    }
+                    stack.Pop();
+                }
+            }
+            return stack.Count ==0;
+        }
+        static void Main2121(string[] args)
+        {
+            Console.WriteLine("문자를 입력해주세요");
+            string pv = Console.ReadLine();
+
+            bool test = Test2(pv);
+
+            if (test)
+            {
+                Console.WriteLine("올바르게 입력되었습니다.");
+            }
+            else
+            {
+                Console.WriteLine("잘못 입력하였습니다. 다시 입력해주세요");
+            }
+        }
+    }
+}
---
+++ Stack_Problem/Queue_Problem2.cs
@@ -0,0 +1,61 @@
+﻿namespace Stack_Problem
+{
+    public class Queue_Problem2
+    {
+        static int[] Test (int[] input)
+        {
+            const int workDay = 8;  
+            
+            int Day = 1;  
+            int work = workDay;  
+            
+            Queue<int> test = new Queue<int>(input);  
+            List<int> endTest = new List<int>();  
+
+            while (test.Count > 0)
+            {
+                int Hours = test.Dequeue();  
+
+                while (Hours > 0)
+                {
+                    if (Hours <= work)
+                    {
+                        
+                        work -= Hours;
+                        Hours = 0;
+                    }
+                    else
+                    {
+                        
+                        Hours -= work;
+                        Day++;  
+                        work = workDay;  
+                    }
+                }
+
+                
+                endTest.Add(Day);
+            }
+
+            return endTest.ToArray();
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] work = { 7, 2, 12, 10, 8, 6 };
+            int[] result = Test(work);
+
+
+        for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i]);
+                if (i < result.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
