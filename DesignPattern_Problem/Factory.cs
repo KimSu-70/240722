4604f1bb6567639243d2ece43e421fed29a0fa84
KimSu-70 <rlatngh4770@naver.com>
2024-08-12 10:05:24 +0900

    디자인패턴 실습과제

---
+++ DesignPattern_Problem/Factory.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Problem
+{
+    //미완성입니다.
+
+
+
+
+    // 과제 1. 팩토리 메소드 패턴 활용하기
+    // 이번 강의에선 팩토리 메소드 패턴의 예시로, 게임에 필요한 오브젝트를 생성하는 코드를 다루었었습니다.
+    // 해당 개념을 참고하여, ItemFactory 클래스를 만들고, 팩토리 메소드를 통해서 다양한 Item을 생성해보세요.
+    //
+    // Item은 ItemFactory를 통해서 생성하도록 한다.
+    //
+    // ItemFactory는 Create(ItemType type) 함수를 가진다.
+    //
+    // ItemType은 열거형으로 Potion, Weapon, Armor, Food 항목을 가진다
+    //
+    // ItemFactory는 Create 함수에 매개변수로 받은 열거형의 유형을 기준으로
+    // 각 항목에 맞는 아이템 인스턴스를 생성하여 준다.
+    public enum ItemType{ Potion, Weapon, Armor, Food }
+    public class ItemFactory
+    {
+        public static Item item(ItemType itemType)
+        {
+            if (itemType == ItemType.Potion)
+            {
+                Potion potion = new Potion();
+                potion.name = "포션";
+                potion.image = "유리병에 담긴 빨간액체";
+                potion.gold = 50;
+                return potion;
+            }
+            else if (itemType == ItemType.Weapon)
+            {
+                Weapon weapon = new Weapon();
+                weapon.name = "롱소드";
+                weapon.image = "무딘 롱소드";
+                weapon.attack = 10;
+                weapon.gold = 100;
+                return weapon;
+            }
+            
+            else if (itemType == ItemType.Armor)
+            {
+                Armor armor = new Armor();
+                armor.name = "천갑옷";
+                armor.image = "해진 천갑옷";
+                armor.defense = 5;
+                armor.gold = 70;
+                return armor;
+            }
+            
+            else if (itemType == ItemType.Food)
+            {
+                Food food = new Food();
+                food.name = "샌드위치";
+                food.image = "먹음직스러운 샌드위치";
+                food.gold = 20;
+                return food;
+            }
+        }
+    }
+    public class Item
+    {
+        public string name;
+        public string image;
+        public int gold;
+        public ItemType itemType;
+        public Item()
+        {
+            this.name = "아이템";
+            this.image = "기본 이미지";
+            this.gold = 0;
+            this.itemType = ItemType.Potion;
+        }
+    }
+
+    public class Potion : Item
+    {
+        public Potion()
+        {
+            this.itemType = ItemType.Potion;
+        }
+    }
+
+    public class Weapon : Item
+    {
+        public int attack;
+
+        public Weapon()
+        {
+            this.itemType = ItemType.Weapon;
+        }
+    }
+
+    public class Armor : Item
+    {
+        public int defense;
+
+        public Armor()
+        {
+            this.itemType = ItemType.Armor;
+        }
+    }
+
+    public class Food : Item
+    {
+        public Food()
+        {
+            this.itemType = ItemType.Food;
+        }
+    }
+
+    internal class Factory
+    {
+        static void Main(string[] args)
+        {
+            Item potion = ItemFactory.item(ItemType.Potion);
+            Item weapon = ItemFactory.item(ItemType.Weapon);
+            Item armor = ItemFactory.item(ItemType.Armor);
+            Item Food = ItemFactory.item(ItemType.Food);
+        }
+    }
+}
---
+++ DesignPattern_Problem/Program.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace DesignPattern_Problem
+{
+    // 과제 2. 빌더패턴 활용하기
+    // 이번 강의에선 빌더패턴의 예시로, 게임에 필요한 오브젝트를 생성하는 코드를 다루었었습니다.
+    // 해당 개념을 참고하여, 원하는 Builder클래스를 만들고, 빌더를 통해서 다양한 객체를 생성해보세요.
+    //
+    // 아래는 예시입니다. 예시를 참고하여 원하는 Builder를 만들어 보세요
+    //
+    // MonsterBuilder 클래스를 만들어 몬스터의 이름, 외형, 전리품, 경험치, 공격범위, 전투스타일 등을 조합하여
+    // 여러 유형의 Monster들을 만들 수 있습니다.
+    public class MonsterBuilder
+    {
+        public string name;
+        public string image;
+        public string booty;
+        public int exp;
+        public float attackRange;
+        public string battleStyle;
+
+        public MonsterBuilder()
+        {
+            this.name = "오크";
+            this.image = "기본 이미지";
+            this.booty = "몽둥이";
+            this.exp = 15;
+            this.attackRange = 1.5f;
+            this.battleStyle = "저돌적인 스타일";
+        }
+
+        public MonsterBuilder SetName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public MonsterBuilder SetImage(string imasge)
+        {
+            this.image = imasge;
+            return this;
+        }
+
+        public MonsterBuilder SetBooty(string booty)
+        {
+            this.booty = booty;
+            return this;
+        }
+        public MonsterBuilder SetExp(int exp)
+        {
+            this.exp = exp;
+            return this;
+        }
+        public MonsterBuilder SetAttackRange(float attackRange)
+        {
+            this.attackRange = attackRange;
+            return this;
+        }
+        public MonsterBuilder SetBattleStyle(string battleStyle)
+        {
+            this.battleStyle = battleStyle;
+            return this;
+        }
+        public void screen()
+        {
+            Console.WriteLine($"몬스터 이름 : {name}");
+            Console.WriteLine($"공격범위 : {attackRange}");
+        }
+    }
+    // AnimalBuilder 클래스를 만들어 동물의 이름, 외형, 생산품, 울음소리, 사료종류, 등을 조합하여
+    // 여러 유형의 Animal들을 만들 수 있습니다.
+    public class AnimalBuilder
+    {
+        public string name;
+        public string image;
+        public string booty;
+        public string whine;
+        public string feedtype;
+
+        public AnimalBuilder()
+        {
+            this.name = "똥개";
+            this.image = "노란색강아지";
+            this.booty = "똥";
+            this.whine = "멍멍";
+            this.feedtype = "싸구려 사료";
+        }
+
+        public AnimalBuilder SetName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public AnimalBuilder SetImage(string imasge)
+        {
+            this.image = imasge;
+            return this;
+        }
+
+        public AnimalBuilder SetBooty(string booty)
+        {
+            this.booty = booty;
+            return this;
+        }
+        public AnimalBuilder SetWhine(string whine)
+        {
+            this.whine = whine;
+            return this;
+        }
+        public AnimalBuilder SetFeedType(string feedtype)
+        {
+            this.feedtype = feedtype;
+            return this;
+        }
+
+        public void screen()
+        {
+            Console.WriteLine($"동물(이름) : {name}");
+            Console.WriteLine($"울음소리 : {whine}");
+            Console.WriteLine($"필요사료 : {feedtype}");
+        }
+    }
+
+    // UnitBuilder 클래스를 만들어 유닛의 이름, 이동방법, 공격타입, 방어타입, 생산가격 등을 조합하여
+    // 여러 유형의 Unit들을 만들 수 있습니다.
+    public enum AttackType { normal, fire, ice, electric }
+    public enum DefenseType { normal, fire, ice, electric }
+    public class UnitBuilder
+    {
+        public string name;
+        public string move;
+        AttackType attackType;
+        DefenseType defenseType;
+        public int gold;
+
+        public UnitBuilder()
+        {
+            this.name = "마린";
+            this.move = "땅으로만 이동가능";
+            this.attackType = AttackType.normal;
+            this.defenseType = DefenseType.normal;
+        }
+
+        public UnitBuilder SetName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public UnitBuilder SetMove(string move)
+        {
+            this.move = move;
+            return this;
+        }
+
+        public UnitBuilder SetAttackType(AttackType attackType)
+        {
+            this.attackType = attackType;
+            return this;
+        }
+        public UnitBuilder SetDefenseType(DefenseType defenseType)
+        {
+            this.defenseType = defenseType;
+            return this;
+        }
+        public UnitBuilder SetGold(int gold)
+        {
+            this.gold = gold;
+            return this;
+        }
+
+        public void screen()
+        {
+            Console.WriteLine($"유닛 : {name}");
+            Console.WriteLine($"이동범위 : {move}");
+            Console.WriteLine($"공격타입 : {attackType}");
+            Console.WriteLine($"방어타입 : {defenseType}");
+            Console.WriteLine($"골드 : {gold}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MonsterBuilder warriorOrcBuilde = new MonsterBuilder();
+            warriorOrcBuilde.SetName("오크 전사");
+            warriorOrcBuilde.SetImage("오크 전사 이미지");
+            warriorOrcBuilde.SetExp(30);
+
+            AnimalBuilder dog = new AnimalBuilder();
+            dog.SetName("누렁이");
+            dog.SetFeedType("고급사료");
+
+            UnitBuilder hellboy = new UnitBuilder();
+            hellboy.SetName("헬보이");
+            hellboy.SetMove("땅,하늘 둘다 이동 가능");
+            hellboy.SetAttackType(AttackType.fire);
+            hellboy.SetDefenseType(DefenseType.fire);
+            hellboy.SetGold(500);
+
+            warriorOrcBuilde.screen();
+            Console.WriteLine();
+            dog.screen();
+            Console.WriteLine();
+            hellboy.screen();
+
+        }
+    }
+}
